eecde9c9525c2112d64d9798addac4daf57bbc10
bmartinslx <brunoemartinslx@gmail.com>
2020-11-05 16:36:59 -0500

    working

--- app/src/main/java/com/example/firebasecrud/MainActivity.java
+++ app/src/main/java/com/example/firebasecrud/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.firebasecrud;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
